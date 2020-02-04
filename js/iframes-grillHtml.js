6bdff236821f0b0eb92871a8d0936c39e9203f6b
MartinM15 <jmmurilloj@hotmail.com>
2020-02-04 16:22:08 -0600

    agregar para todo el equipo

---
+++ js/iframes-grillHtml.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+
+    $("#btnGraficas").click(function(e) {
+
+        var frame = $('#FrameId');
+        var url = 'Graficas1.html';
+        frame.attr('src', url).show();
+
+    });
+    $("#btnPanelUser").click(function(e) {
+
+        var frame = $('#FrameId');
+        var url = 'UsuariosGrid.html';
+        frame.attr('src', url).show();
+
+    });
+    $("#btnPanelMaquinas").click(function(e) {
+
+        var frame = $('#FrameId');
+        var url = 'EquiposGrid.html';
+        frame.attr('src', url).show();
+
+    });
+    $(function() {
+        $('#datetimepicker1').datetimepicker();
+    });
+
+
+
+});
