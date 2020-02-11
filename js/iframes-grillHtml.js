be63b14287ea897707b96ed436d9d773cb1036cd
MartinM15 <jmmurilloj@hotmail.com>
2020-02-10 19:44:33 -0600

    se agrega nuevo grid evidencias y nuevo login

--- js/iframes-grillHtml.js
+++ js/iframes-grillHtml.js
@@ -28,9 +28,49 @@ $(document).ready(function() {
         frame.attr('src', url).show();
 
     });
-    $(function() {
-        $('#datetimepicker1').datetimepicker();
+    $("#btnEvidenciasMantto").click(function(e) {
+        var frame = $('#FrameId');
+        var url = 'EvidenciasGrid.html';
+        frame.attr('src', url).show();
     });
+
+    $('#inputGroupFile01').change(function(e) {
+
+        var file = e.target.files[0],
+            imageType = /image.*/;
+
+        if (!file.type.match(imageType))
+            return;
+
+        var reader = new FileReader();
+
+        reader.onload = function(e) {
+            var result = e.target.result;
+            $('#imgSalidaAntes').attr("src", result);
+        }
+
+        reader.readAsDataURL(file);
+
+    });
+    $('#inputGroupFile02').change(function(e) {
+        var file = e.target.files[0],
+            imageType = /image.*/;
+
+        if (!file.type.match(imageType))
+            return;
+
+        var reader = new FileReader();
+
+        reader.onload = function(e) {
+            var result = e.target.result;
+            $('#imgSalidaDespues').attr("src", result);
+        }
+
+        reader.readAsDataURL(file);
+    });
+
+
+
 
 
 
